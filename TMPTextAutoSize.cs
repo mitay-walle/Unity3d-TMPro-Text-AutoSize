40eb81f0e1e54463956356d354a71b637d28971c
Dmitry <mitay-walle91@mail.ru>
2022-10-07 08:25:46 +0300

    #1 fixed, TMP_InputField | fix IndexOutOfRange at AddComponent | OnValidate | Size edited at component | Collect Components at AddComponent | ContextMenu Buttons 'Validate Values' and 'Collect Components'

--- TMPTextAutoSize.cs
+++ TMPTextAutoSize.cs
@@ -1,61 +1,61 @@
-﻿/// based on this: https://forum.unity.com/threads/textmeshpro-precull-dorebuilds-performance.762968/#post-5083490
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using TMPro;
+using UnityEditor;
 
 namespace Plugins.mitaywalle.UI.Layout
 {
-    public enum eResizePattern
+    public enum ResizePattern
     {
+        /// <summary>
+        /// fast, balanced quality
+        /// </summary>
         IgnoreRichText,
+
+        /// <summary>
+        /// fast, simple
+        /// </summary>
         AllCharacters,
     }
 
     [ExecuteAlways]
     public class TMPTextAutoSize : MonoBehaviour
     {
+        private const string _tooltipText = "IgnoreRichText - fast, balanced\nqualityAllCharacters - fast, simple";
+
+        [SerializeField, Tooltip(_tooltipText)]
+        private ResizePattern _pattern;
+
+        [SerializeField] private Vector2 _size = new Vector2(18, 72);
+        [SerializeField] private bool _executeOnUpdate = true;
         [SerializeField] private List<TMP_Text> _labels = new List<TMP_Text>();
-        [SerializeField] private eResizePattern _pattern;
-        [SerializeField] private bool _executeOnUpdate;
+        [SerializeField] private List<TMP_InputField> _inputs = new List<TMP_InputField>();
         private int _currentIndex;
 
         private void Update()
         {
             if (_executeOnUpdate) Execute();
-
             OnUpdateCheck();
         }
 
         public void Execute()
         {
-            if (_labels.Count == 0) return;
+            if (_labels.Count == 0 && _inputs.Count == 0) return;
 
             int count = _labels.Count;
-
             int index = 0;
             float maxLength = 0;
 
             for (int i = 0; i < count; i++)
             {
-                float length = 0;
-
-                switch (_pattern)
+                float length = _pattern switch
                 {
-                    case eResizePattern.IgnoreRichText:
-                        length = _labels[i].GetParsedText().Length;
-
-                        break;
-
-                    case eResizePattern.AllCharacters:
-                        length = _labels[i].text.Length;
-
-                        break;
-
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
+                    ResizePattern.IgnoreRichText => _labels[i].GetParsedText().Length,
+                    ResizePattern.AllCharacters => _labels[i].text.Length,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
 
                 if (length > maxLength)
                 {
@@ -69,32 +69,52 @@ namespace Plugins.mitaywalle.UI.Layout
                 OnChanged(index);
             }
         }
+
         private void OnChanged(int index)
         {
-            // Disable auto size on previous
             _labels[_currentIndex].enableAutoSizing = false;
-
             _currentIndex = index;
-
-            // Force an update of the candidate text object so we can retrieve its optimum point size.
+            _labels[index].fontSizeMin = _size.x;
+            _labels[index].fontSizeMax = _size.y;
             _labels[index].enableAutoSizing = true;
             _labels[index].ForceMeshUpdate();
         }
+
         private void OnUpdateCheck()
         {
+            if (_labels.Count == 0 && _inputs.Count == 0) return;
             float optimumPointSize = _labels[_currentIndex].fontSize;
-
-            // Iterate over all other text objects to set the point size
             int count = _labels.Count;
 
             for (int i = 0; i < count; i++)
             {
                 if (_currentIndex == i) continue;
-
                 _labels[i].enableAutoSizing = false;
-
                 _labels[i].fontSize = optimumPointSize;
             }
+
+            count = _inputs.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                _inputs[i].pointSize = optimumPointSize;
+            }
         }
+
+        [ContextMenu("Collect Child Components")]
+        public void CollectChildComponents()
+        {
+            #if UNITY_EDITOR
+            Undo.RecordObject(this, "collect child components");
+            #endif
+
+            _labels = GetComponentsInChildren<TMP_Text>(true).ToList();
+            _inputs = GetComponentsInChildren<TMP_InputField>(true).ToList();
+        }
+
+        private void Reset() => CollectChildComponents();
+
+        [ContextMenu("Validate values")]
+        private void OnValidate() => OnChanged(_currentIndex);
     }
 }
